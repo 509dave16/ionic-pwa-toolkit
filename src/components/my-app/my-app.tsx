3c46d589a222c5750352e07f3a4b671ec61664f8
David Fall <davidfall@rocketmail.com>
2018-06-04 19:30:12 -0700

    Render, query, and use modal controller on load

--- src/components/my-app/my-app.tsx
+++ src/components/my-app/my-app.tsx
@@ -1,5 +1,5 @@
 import '@ionic/core';
-import { Component, Prop, Listen } from '@stencil/core';
+import {Component, Prop, Listen} from '@stencil/core';
 
 @Component({
   tag: 'my-app',
@@ -7,7 +7,7 @@ import { Component, Prop, Listen } from '@stencil/core';
 })
 export class MyApp {
 
-  @Prop({ connect: 'ion-toast-controller' }) toastCtrl: HTMLIonToastControllerElement;
+  @Prop({connect: 'ion-toast-controller'}) toastCtrl: HTMLIonToastControllerElement;
 
   /**
    * Handle service worker updates correctly.
@@ -26,13 +26,51 @@ export class MyApp {
       closeButtonText: 'Reload'
     });
     await toast.present();
-    await toast.onWillDismiss()
+    await toast.onWillDismiss();
     window.location.reload();
   }
+
+  async componentDidLoad() {
+    this.presentModal();
+  }
+
+  presentModal = async () => {
+    // initialize controller
+    const modalController = document.querySelector('ion-modal-controller');
+    await modalController.componentOnReady();
+
+    // create component to open
+    const element = document.createElement('div');
+    element.innerHTML = `
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Super Modal</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <h1>Content of doom</h1>
+        <div>Here's some more content</div>
+        <ion-button class="dismiss">Dismiss Modal</ion-button>
+      </ion-content>
+    `;
+
+    // listen for close event
+    const button = element.querySelector('ion-button');
+    button.addEventListener('click', () => {
+      modalController.dismiss();
+    });
+
+    // present the modal
+    const modalElement = await modalController.create({
+      component: element
+    });
+    modalElement.present();
+  };
 
   render() {
     return (
       <ion-app>
+        <ion-modal-controller></ion-modal-controller>
         <ion-router useHash={false}>
           <ion-route url='/' component='app-home'></ion-route>
           <ion-route url='/profile/:name' component='app-profile'></ion-route>
