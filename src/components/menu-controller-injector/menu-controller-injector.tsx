57bc6b272153db1b84d7bda4cdec649cca4516c4
David Fall <davidfall@rocketmail.com>
2018-06-05 21:49:35 -0700

    Updated injectors with error throws

--- src/components/menu-controller-injector/menu-controller-injector.tsx
+++ src/components/menu-controller-injector/menu-controller-injector.tsx
@@ -9,6 +9,9 @@ export class MenuControllerInjector implements IStencilElementInjector {
   @Method()
   create(): Promise<HTMLIonMenuControllerElement> {
     const element: HTMLIonMenuControllerElement = document.querySelector('ion-menu-controller');
+    if (!element) {
+      throw new Error('Did you forget to render an <ion-menu> element? No ion-menu-controller element exists.');
+    }
     return element.componentOnReady();
   }
 }
--- src/components/nav-injector/nav-injector.tsx
+++ src/components/nav-injector/nav-injector.tsx
@@ -9,6 +9,9 @@ export class NavInjector implements IStencilElementInjector {
   @Method()
   create(): Promise<HTMLIonNavElement> {
     const element: HTMLIonNavElement = document.querySelector('ion-nav');
+    if (!element) {
+      throw new Error('Did you forget to render an <ion-nav> element?');
+    }
     return element.componentOnReady();
   }
 }
